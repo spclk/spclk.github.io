785a57d11170b091737ad6d5d9fb98240c585016
Konstantin <konstantin1020@gmail.com>
2021-07-23 13:47:52 -0700

    styling on Portfolio text

--- src/pages/main.js
+++ src/pages/main.js
@@ -12,7 +12,7 @@ function Main() {
           </h5>
           <h6 className="grey-text text-darken-3 lighten-3">
             I've graduated from the UC Davis Full-Stack Coding Bootcamp, where I've had 
-            a thrilling experience of coding everything from JavaScript to React to Back-End databases. 
+            an amazing experience of coding everything from JavaScript to React to Back-End databases. 
             Out of all the things I love about coding, there are two things I
             love the most. I love how coding takes me on a thrilling ride of
             solving complex challenges. And I most certainly love how coding
@@ -37,7 +37,9 @@ function Main() {
         />
         <div className="section white">
           <div className="row container">
-            <h3 className="header">Portfolio</h3>
+            <h4 className="grey-text text-darken-3 lighten-3">
+            Portfolio
+            </h4>
             <h6 className="grey-text text-darken-3">
               Check out some of the the projects I've done at the UC Davis
               Coding Bootcamp. <br />
